529d1c3c25ec99115f4e77e611cefd9734a05124
magaivaxe <mpg.salgad@gmail.com>
2018-03-02 13:06:16 -0500

    Button works

--- src/MaFenetre.java
+++ src/MaFenetre.java
@@ -7,6 +7,8 @@
 
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
@@ -14,7 +16,7 @@ import javax.swing.JFrame;
  * 
  * @author sire_marcos
  */
-public class MaFenetre extends JFrame
+public class MaFenetre extends JFrame implements ActionListener
 {
     //Mouse object
     EcouterSouris souris = new EcouterSouris();
@@ -38,10 +40,23 @@ public class MaFenetre extends JFrame
         setBounds(10, 20, 600, 600);
         //Button sets
         monButton = new JButton("Essai!");
+        //Set the layout to button
         cont.setLayout(new FlowLayout());
+        //Add the button to window container
         cont.add(monButton);
+        //Add an action listener to button and the listener is MaFenetre itself
+        monButton.addActionListener(this);
         //Show all
         setVisible(true);
+    }
+    /**
+     * button action
+     * @param e event or action performed by button
+     */
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        System.out.println("action sur button Essai");
     }
 
     
